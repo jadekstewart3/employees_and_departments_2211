4ced659a122cc41605af3653553f98a3c577507f
Jade Stewart <jadekstewart3@gmail.com>
2023-01-13 10:02:43 -0700

    Create test for expenses_less_than_500 method

--- lib/budget.rb
+++ lib/budget.rb
@@ -1,9 +1,16 @@
 class Budget
   attr_reader :year,
-              :total_budget
+              :total_budget,
+              :departments
         
   def initialize(budget_info)
     @year = budget_info[:year].to_i
     @total_budget = budget_info[:total_budget][1..-1].to_i
+    @departments = []
   end
+
+  def add_department(department)
+    @departments.push(department)
+  end
+  
 end
--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -30,4 +30,20 @@ RSpec.describe do
       expect(budget.departments).to eq([customer_service, it])
     end
   end
+
+  describe '#expenses_less_than_500' do 
+    it 'returns a has with expenses less and 500 by department' do 
+      budget.add_department(customer_service)
+      budget.add_department(it)
+
+      customer_service.expense(100)
+      customer_service.expense(25)
+
+      it.expense(550)       
+      it.expense(75) 
+      
+      
+      expect(budget.expenses_less_than_500).to eq([customer_service])
+    end
+  end
 end
