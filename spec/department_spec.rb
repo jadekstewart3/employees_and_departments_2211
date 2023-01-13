67d9140460ba401566694cde1f80c6c2b64d6027
Jade Stewart <jadekstewart3@gmail.com>
2023-01-13 09:19:12 -0700

    Create initialize test

---
+++ spec/department_spec.rb
@@ -0,0 +1,22 @@
+require './lib/employee'
+require './lib/department'
+
+RSpec.describe do 
+
+  let(:customer_service) { Department.new("Customer Service") }
+
+  let(:bobbi) { Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"}) }
+  let(:aaron) { Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"}) }
+
+
+  describe '#initialize' do 
+    it 'exists' do 
+      expect(customer_service).to be_a(Department)
+    end
+
+    it 'has attributes' do 
+      expect(customer_service.name).tp eq("Customer Service")
+      expect(customer_service.employees).to eq([])
+    end
+  end
+end
