4fec5e885231b10252493ee05eb7437099aedb73
Jade Stewart <jadekstewart3@gmail.com>
2023-01-13 09:53:26 -0700

    Create test for add_department method

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -3,21 +3,31 @@ require './lib/department'
 require './lib/budget'
 
 RSpec.describe do 
-  let(:customer_service_budget) { Budget.new({year: "2023" total_budget: "$400000"})}
+  let(:budget) { Budget.new({year: "2023", total_budget: "$100000000"})}
 
   let(:customer_service) { Department.new("Customer Service") }
+  let(:it) { Department.new("I.T.") }
 
   let(:bobbi) { Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"}) }
   let(:aaron) { Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"}) }
 
   describe '#initialize' do 
     it 'exists' do 
-      expect(customer_service_budget).to be_a(Budget)
+      expect(budget).to be_a(Budget)
     end
 
     it 'has attributes' do 
-      expect(customer_service_budget.year).to eq(2023)
-      expect(customer_service_budget.total_budget).to eq(400000)
+      expect(budget.year).to eq(2023)
+      expect(budget.total_budget).to eq(100000000)
+    end
+  end
+
+  describe '#add_department' do 
+    it 'adds departments to the departments array' do
+      budget.add_department(customer_service)
+      budget.add_department(it)
+
+      expect(budget.departments).to eq([customer_service, it])
     end
   end
 end
